4d124d5561d9e893de947870673e4155a0e1c0e9
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-07 02:06:37 +0100

    style: fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import './globals.css'
-import { Noto_Sans_JP } from 'next/font/google'
-import Link from 'next/link'
-import Header from '@/components/header'
 import Footer from '@/components/footer'
 
-const notoSans = Noto_Sans_JP({
-  weight: ['400', '600'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
 export const metadata = {
-  title: 'TiRO | Geekのためのメルマガ | 毎朝５分のインプット',
+  title: 'TiRO | Geekのための 📩 毎朝５分のインプット',
   description: `最新記事を読む時間がない。英語だと尚更。TiROは、そんなGeekのための無料メルマガです。
     テクノロジーやプログラミングに関する興味深い記事を、要約・翻訳して 毎朝お届けします。`,
 }
@@ -21,7 +11,12 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 
   return (
     <html lang="en">
-      <body className={notoSans.className + " flex min-h-screen flex-col items-center justify-between sm:text-base"}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500&display=swap" rel="stylesheet"/>
+      </head>
+      <body className="flex min-h-screen flex-col items-center justify-between text-base">
         {children}
         <Footer/>
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontSize: {
+      sm: '0.80rem',
+      base: '0.9375rem',
+      xl: '1.10rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     },
   },
   plugins: [],
