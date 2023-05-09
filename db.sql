64e32aeade1eefa6c088f4862b74e44334ea815b
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-09 20:12:00 +0100

    Note: db init

---
+++ db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE subscriber (
+	email_address varchar(255) NOT NULL,
+	verified int DEFAULT 0 NOT NULL,
+	tech_subscribed int NOT NULL,
+	web_subscribed int NOT NULL,
+	ai_subscribed int NOT NULL,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO subscriber
+VALUES (
+	'test@test.com',
+	0,
+	1,
+	0,
+	0
+)
