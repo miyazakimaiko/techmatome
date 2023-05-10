fc96e38dc24cb4d5b7559613214a3f612b1286d2
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-11 00:43:59 +0100

    Match data type with backend

--- frontend/interfaces/subscriber.tsx
+++ frontend/interfaces/subscriber.tsx
@@ -1,7 +1,7 @@
 export interface Subscriber {
-  email: string
-  verified: boolean
-  tech: boolean
-  web: boolean
-  ai: boolean
+  email_address: string
+  verified: 0 | 1
+  tech_subscribed: 0 | 1
+  web_subscribed: 0 | 1
+  ai_subscribed: 0 | 1
 }
