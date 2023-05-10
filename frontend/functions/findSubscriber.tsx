932c99028336d4eed3e7bc62afc3b5cbdf3ef48e
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-11 00:40:50 +0100

    Change folder name

---
+++ frontend/functions/findSubscriber.tsx
@@ -0,0 +1,17 @@
+import Error from "next/error"
+import { FindSubscriberPayload } from "@/interfaces/findSubscriberPayload"
+
+export default async function findSubscriber (email: string): Promise<FindSubscriberPayload> {
+  try {
+    const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT
+    
+    const response = await fetch(
+      `${apiEndpoint}/find?email=${email}`, 
+      {  method: "GET" }
+    ) 
+    return await response.json() as FindSubscriberPayload
+
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
--- frontend/helpers/addSubscriber.tsx
+++
@@ -1,9 +0,0 @@
-import { Subscriber } from "@/interfaces/subscriber"
-
-export default function addSubscriber (subscriber: Subscriber): Subscriber {
-  console.log("subscribed: ", subscriber)
-  subscriber.tech = true;
-  subscriber.verified = false;
-  
-  return subscriber
-}
