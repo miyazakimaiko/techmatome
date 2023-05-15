cfb4906604840b4bb9a1c63ec2b86caef63b6654
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-15 23:51:02 +0100

    add: new table for email verification

--- db.sql
+++ db.sql
@@ -4,7 +4,15 @@ CREATE TABLE subscriber (
 	tech_subscribed int NOT NULL,
 	web_subscribed int NOT NULL,
 	ai_subscribed int NOT NULL,
-	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE email_verification_token (
+  email_address varchar(255),
+  encrypted_token text NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (email_address, encrypted_token),
+	FOREIGN KEY (email_address) REFERENCES subscriber(email_address) ON DELETE CASCADE
 );
 
 INSERT INTO subscriber
