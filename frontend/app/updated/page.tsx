1541d6decb6fece4a5d16b6345fd8d95e3fea2e6
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-15 23:48:41 +0100

    feature: verify email

--- frontend/app/updated/page.tsx
+++ frontend/app/updated/page.tsx
@@ -1,25 +1,12 @@
 "use client"
 import Link from "next/link"
-import { useEffect, useState } from "react"
-import { useSearchParams } from "next/navigation"
-import validateEmail from "@/functions/validateEmail"
 
 export default function Updated() {
-  const searchParams = useSearchParams()
-  const email = searchParams.get("email")
-  const [emailValidated, setEmailValidated] = useState(false)
-
-  useEffect(() => {
-    if (email) {
-      validateEmail(email).then(() => setEmailValidated(true))
-    }
-  }, [email])
   
   return (
     <>
       <main>
         <h1 className="page-title">登録内容を変更しました。</h1>
-        <p>登録確認のメールを {emailValidated ? `${email} へ` : null} 送信しました。</p>
         <div className="max-w-[200px] mx-auto my-8 flex flex-col underline">
           <Link href="/archives">過去のメルマガを読んでみる</Link>
           <Link href="">広告を載せる</Link>
---
+++ frontend/app/verify/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useSearchParams } from "next/navigation"
+import verifySubscriber from "@/functions/verifySubscriber"
+
+export default function Verify() {
+  const searchParams = useSearchParams()
+  const email = searchParams.get("email")
+  const token = searchParams.get("token")
+  const [processing, setProcessing] = useState(true)
+  const [verified, setVerified] = useState(false)
+
+  useEffect(() => {
+    async function verify(email: string, token: string) {
+      const res = await verifySubscriber(email, token)
+      setVerified(res.success)
+      setProcessing(false)
+    }
+    if (email && token) {
+      verify(email, token)
+    }
+  }, [email, token])
+  
+  return (
+    <>
+      <main>
+        <p>
+          {processing 
+            ? `確認しています...` 
+            : verified 
+              ? `登録完了しました🎉` 
+              : `【無効のリンク】再び登録の手順を踏んでいただくか、support@tiro.newsへご連絡ください。`}
+        </p>
+        {verified 
+          ? <div className="max-w-[200px] mx-auto my-8 flex flex-col underline">
+              <Link href="/archives">過去のメルマガを読んでみる</Link>
+              <Link href="">広告を載せる</Link>
+              <Link href="">仕事を探す</Link>
+              <Link href="">仕事を載せる</Link>
+            </div>
+          : null}
+      </main>
+    </>
+  )
+}
