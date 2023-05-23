25b8589d6ff877991f681aa952ded7f3b904adaa
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-23 23:36:21 +0100

    add: config & ses stack

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,10 @@
 import { SSTConfig } from "sst"
+import { ConfigStack } from "./stacks/ConfigStack"
 import { AuroraStack } from "./stacks/AuroraStack"
 import { BucketStack } from "./stacks/BucketStack"
 import { CronStack } from "./stacks/CronStack"
 import { SnsStack } from "./stacks/SnsStack"
+import { SesStack } from "./stacks/SesStack"
 import { ApiStack } from "./stacks/ApiStack"
 import { SiteStack } from "./stacks/SiteStack"
 
@@ -14,10 +16,12 @@ export default {
     };
   },
   stacks(app) {
+    app.stack(ConfigStack)
     app.stack(AuroraStack)
     app.stack(BucketStack)    
     app.stack(CronStack)    
     app.stack(SnsStack)
+    app.stack(SesStack)
     app.stack(ApiStack)
     app.stack(SiteStack)
   },
