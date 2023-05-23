08f3da09a260d467776b5c9949e82fbcd5791c0e
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-23 23:35:58 +0100

    fix: enable link target

--- frontend/app/archives/[...slug]/content.tsx
+++ frontend/app/archives/[...slug]/content.tsx
@@ -1,18 +1,8 @@
 "use client"
 import Markdown from "markdown-to-jsx"
-import { useLayoutEffect } from "react"
 
 export default function Content(props: any) {
   const content = props.content
-
-  // * To force open links in new tab
-  useLayoutEffect(() => {
-    const article = document.getElementsByTagName('article')[0]
-    const links = article.getElementsByTagName('a')
-    for (let link of links) {
-      link.target = "_blank"
-    }
-  }, [])
 
   return (
     <>
