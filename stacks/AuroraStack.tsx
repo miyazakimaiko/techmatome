7abd418156d15d58c155aec434d91a99477e2884
miyazakimaiko <myzkmik19922@gmail.com>
2023-05-28 11:24:52 +0100

    fix: slow start on first query

--- stacks/AuroraStack.tsx
+++ stacks/AuroraStack.tsx
@@ -7,6 +7,9 @@ export function AuroraStack({ stack }: StackContext) {
   const cluster = new RDS(stack, "Cluster", {
     engine: "postgresql11.13",
     defaultDatabaseName: DATABASE_NAME,
+    scaling: {
+      minCapacity: "ACU_2"
+    }
   })
 
   new Script(stack, "auroraCreateTableScript", {
