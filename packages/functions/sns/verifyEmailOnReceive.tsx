96209bf7d34f6416033e2ce357b2435abb5df733
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-05 23:40:11 +0100

    debugging prod issue

--- packages/functions/sns/verifyEmailOnReceive.tsx
+++ packages/functions/sns/verifyEmailOnReceive.tsx
@@ -1,8 +1,12 @@
 import { TiroRds } from "core/rds"
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns"
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses"
 import { Topic } from "sst/node/topic"
+import getWelcomeTemplates from "templates/welcome"
+import libmime from "libmime"
 
 const sns = new SNSClient({ region: "eu-west-1" })
+const ses = new SESClient({ region: "eu-west-1" })
 
 declare module "sst/node/topic" {
   export interface TopicResources {
@@ -20,20 +24,14 @@ export async function handler(event: any) {
       obj.name === "Subject" || obj.name === "From"
     ))
 
-    const res = {
-      subject: "",
-      sender: "",
-    }
-    for (const item of filtered) {
-      if (item.name === "Subject") {
-        res.subject = item.value
-      }
-      else if (item.name === "From") {
-        const regExpForEmail = /\<([^)]+)\>/g
-        const email = regExpForEmail.exec(item.value)
-        res.sender = email ? email[1] : item.value
-      }
-    }
+    console.log({filtered})
+
+    const res = await getSenderAndSubject(filtered)
+
+    console.log({res})
+
+    console.log('res.subject.includes("登録"): ', res.subject.includes("登録"))
+    console.log('res.subject.includes("完了してください"): ', res.subject.includes("完了してください"))
 
     if (res.subject.includes("登録") && res.subject.includes("完了してください")) {
       verify(res.sender)
@@ -44,23 +42,82 @@ export async function handler(event: any) {
   }
 }
 
-async function verify(email: string) {
-  const updateResult = await TiroRds.db
-  .updateTable("subscriber")
-  .set({ verified: 1 })
-  .where("email_address", "=", email)
-  .returning("email_address")
-  .execute();
-
-  const targetEmail = updateResult[0].email_address
-
-  if (targetEmail) {
-    // Publish SNS to be picked up by lambda emailing verified message
-    const command = new PublishCommand({ 
-      TopicArn: Topic.SubscriberVerifiedTopic.topicArn,
-      Message: JSON.stringify({ email: targetEmail }),
-    })
-    await sns.send(command)
+async function getSenderAndSubject(filtered: any) {
+  const res = {
+    subject: "",
+    sender: "",
   }
-  console.log("email verified: ", targetEmail)
+  for await (const item of filtered) {
+    if (item.name === "Subject") {
+      res.subject = item.value
+    }
+    else if (item.name === "From") {
+      const regExpForEmail = /\<([^)]+)\>/g
+      const email = regExpForEmail.exec(item.value)
+      res.sender = email ? email[1] : item.value
+    }
+  }
+  return res
+}
+
+async function verify(email: string) {
+  try {    
+    console.log("STARTED VERIFY FUNCTION: ", email)
+    console.log({TiroRds})
+  
+    // const updateResult = await TiroRds.db
+    // .updateTable("subscriber")
+    // .set({ verified: 1 })
+    // .where("email_address", "=", email)
+    // .execute();
+  
+    console.log("DEBUGGING: targetEmail: ", email)
+  
+    // Publish SNS to be picked up by lambda emailing verified message
+    console.log("========= publishing topic command to send verified email =======")
+    await sendVerifiedNotification(email)
+    console.log("email verified: ", email)
+
+  } catch (error) {
+    console.error("error: ", error)
+    throw error
+  }
+}
+
+export async function sendVerifiedNotification(email: string) {
+  
+  const { html, text } = await getWelcomeTemplates(email)
+  const senderName = "Techまとめ"
+
+  const emailCommand = new SendEmailCommand({
+    Destination: {
+      CcAddresses: [],
+      ToAddresses: [email]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: html
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: text
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "【登録完了しました！】Techまとめニュースレター"
+      }
+    },
+    Source: `${libmime.encodeWord(senderName)} <miyazaki@techmatome.com>`
+  })
+
+  try {
+    await ses.send(emailCommand)
+    console.log("INFO: Email has been verified and sent welcome email.")
+
+  } catch (error) {
+    console.error(error)
+  }
 }
