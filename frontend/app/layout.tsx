0168797447a848f194fc6f03d6cfaf0b9706a3ec
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-10 22:05:07 +0100

    feature: add google analytics tag

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 import Footer from '@/components/footer'
 
 export const metadata = {
@@ -16,6 +17,20 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500&display=swap" rel="stylesheet"/>
       </head>
+      {/* Google tag (gtag.js) */}
+      <Script 
+        src="https://www.googletagmanager.com/gtag/js?id=G-1CNJZG7XNH"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1CNJZG7XNH');
+        `}
+      </Script>
       <body 
         suppressHydrationWarning={true}
         className="flex min-h-screen flex-col items-center justify-between text-base leading-7"
