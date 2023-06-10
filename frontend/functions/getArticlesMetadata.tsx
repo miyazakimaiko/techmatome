9454ea62735000a675f8f9649edae8e0b3082c80
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-10 22:07:11 +0100

    fix: stop process if there's no articles

--- frontend/functions/getArticlesMetadata.tsx
+++ frontend/functions/getArticlesMetadata.tsx
@@ -7,25 +7,31 @@ const root = process.cwd()
 
 export default function getArticlesMetadata (category: string): ArticleMetadata[] {
 
+  let articles: ArticleMetadata[] = [];
   const folder = `public/articles/${category}/`
   const source = path.join(root, folder)
 
   const files = fs.readdirSync(source)
-  const markdownArticles = files.filter((file) => file.endsWith(".md"))
 
-  // Get gray-matter data from each file.
-  const articles = markdownArticles.map((fileName) => {
-    const fileContents = fs.readFileSync(`${source}${fileName}`, "utf8")
-    const matterResult = matter(fileContents)
-    return {
-      title: matterResult.data.subject,
-      subject: matterResult.data.subject,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle ?? "No subtitle",
-      slug: fileName.replace(".md", ""),
-      category,
+  if (files.length > 0) {
+    const markdownArticles = files.filter((file) => file.endsWith(".md"))
+  
+    if (markdownArticles.length > 0) {
+      // Get gray-matter data from each file.
+      articles = markdownArticles.map((fileName) => {
+        const fileContents = fs.readFileSync(`${source}${fileName}`, "utf8")
+        const matterResult = matter(fileContents)
+        return {
+          title: matterResult.data.subject,
+          subject: matterResult.data.subject,
+          date: matterResult.data.date,
+          subtitle: matterResult.data.subtitle ?? "No subtitle",
+          slug: fileName.replace(".md", ""),
+          category,
+        }
+      })
     }
-  })
+  }
 
   return articles
 }
