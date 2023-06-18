f16b8fc1d6258441019d7f142b79dc2119d57660
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-18 16:20:37 +0100

    refactor: separate response obj from services

---
+++ packages/functions/api/common/responses.tsx
@@ -0,0 +1,27 @@
+export function creationSuccessResponse(type: string) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      success: true,
+      type
+    }),
+  }
+}
+
+export function findSuccessResponse(data: any) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      found: Boolean(data),
+      data
+    }),
+  }
+}
+
+export function errorResponse(e: any) {
+  return {
+    statusCode: e.statusCode ?? 500,
+    type: e.type ?? "InternalServerError",
+    message: e.message ?? "Unknown error",
+  }
+}
