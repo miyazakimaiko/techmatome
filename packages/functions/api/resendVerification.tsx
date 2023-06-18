fd017ef805f4c6b0cc50a484ab81891a33d5fe26
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-18 17:03:42 +0100

    refactor: create ResendVerificationService

--- packages/functions/api/resendVerification.tsx
+++ packages/functions/api/resendVerification.tsx
@@ -1,50 +1,75 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns"
 import { Topic } from "sst/node/topic"
 import { XataClient } from "../../xata"
+import { BadRequestError, NotFoundError } from "./common/errors"
+import { errorResponse, successResponse } from "./common/responses"
 
-const xata = new XataClient({ apiKey: process.env.DB_API_KEY })
-const sns = new SNSClient({ region: "eu-west-1" })
+const xata = new XataClient({ 
+  apiKey: process.env.DB_API_KEY,
+  branch: process.env.DB_BRANCH,
+})
+const sns = new SNSClient({
+  region: "eu-west-1"
+})
 
-export async function handler(event: any) {
-  try {
-    const { email } = event.queryStringParameters
+export function ResendVerificationService(dbClient: any, snsClient: any) {
 
-    const subscribers = await xata.db.subscriber
+  async function validateEventQueryStringParam(event: any) {
+    if (!event.queryStringParameters) {
+      throw new BadRequestError("Missing queryStringParameters")
+    }
+    if (!event.queryStringParameters.email) {
+      throw new BadRequestError("Missing email queryStringParameter")
+    }
+    return event.queryStringParameters
+  }
+
+  async function findSubscriber(email: string) {
+    const subscribers = await dbClient.db.subscriber
       .filter({ email_address: email })
       .getMany()
 
     if (!subscribers[0]) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ 
-          error: "Cannot find subscriber"
-        }),
-      }
+      throw new NotFoundError("Could not find subscriber")
     }
+    return subscribers[0]
+  }
 
-    // Publish SNS to be picked up by lambda sending verification email
+  async function sendSubscriberCreationTopic(email: string) {
     const command = new PublishCommand({ 
       TopicArn: Topic.SubscriberCreationTopic.topicArn,
       Message: JSON.stringify({
-        email: subscribers[0].email_address,
+        email,
       }),
     })
 
-    await sns.send(command)
+    await snsClient.send(command)
+    return true
+  }
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ 
-        success: true,
-        type: "resent" 
-      })
-    }
+  async function main(event: any) {
+    try {
+      const { email } = await validateEventQueryStringParam(event)
+      const subscriber = await findSubscriber(email)
+      await sendSubscriberCreationTopic(subscriber.email_address)
 
-  } catch (e: any) {
-    console.error(e)
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
+      return successResponse("resent")
+
+    } catch (e: any) {
+      return errorResponse(e)
     }
   }
+
+  return {
+    validateEventQueryStringParam,
+    findSubscriber,
+    sendSubscriberCreationTopic,
+    main,
+  }
+}
+
+const service = ResendVerificationService(xata, sns)
+
+export async function handler(event: any) {
+  service.main(event)
 }
