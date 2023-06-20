a41d6e8f12f8fc522d87ebdba1abe2ab5e289f98
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-20 21:37:29 +0100

    refactor: change text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   const title = (
     <div className="flex flex-col items-center justify-between pt-4 sm:pt-8">
       <h1 className="text-3xl font-semibold mb-4">
-        Techまとめ
+        Techまとめ速報
       </h1>
-      <p className="text-sm pt-2">毎朝５分のインプット</p>
+      <p className="text-sm pt-2">5分でサクッと読めるテックメルマガ</p>
     </div>
   )
 
   const emojis = (
-    <div className="w-full max-w-[300px] m-auto flex justify-evenly text-5xl py-10">
+    <div className="w-full max-w-[300px] m-auto flex justify-evenly text-4xl py-10">
       <div className="animate-bounce">⏱</div>
       <span className="animate-bounce animation-bounce-delay">📱</span>
       <span className="animate-bounce">👨🏻‍💻</span>
@@ -22,10 +22,19 @@ export default function Home() {
   )
 
   const introText = (
-    <div className="text-center pb-8">
+    <div className="text-center pb-3">
+      <p className="pb-4">
+        最新情報を手軽に読みたい人、<br/>
+        情報過多に疲れた人のための<br/>
+        完全無料メルマガ📩
+      </p>
+      <p className="pb-4">
+        テクノロジー、プログラミング、<br/>
+        AIに関する英語の最新記事やブログを<br/>
+        「翻訳」「要約」して毎朝お届けします🎁
+      </p>
       <p className="pb-6">
-        無料でサクッと読める、<br/>Geek・エンジニアのためのメルマガ📩
-        <br/>英語の最新記事やブログを翻訳、要約して毎朝お届けします🎁
+        最新情報をもっとシンプルに
       </p>
     </div>
   )
