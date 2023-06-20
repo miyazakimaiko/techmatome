a2c1c29ed7e6c7210cb11f26f9b809ea72378d79
miyazakimaiko <myzkmik19922@gmail.com>
2023-06-20 21:37:17 +0100

    refactor: change text

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,8 +3,8 @@ import Script from 'next/script'
 import Footer from '@/components/footer'
 
 export const metadata = {
-  title: 'Techまとめ | 毎朝５分のインプット',
-  description: `無料でサクッと読める、Geek・エンジニアのためのメルマガ📩 英語の最新記事やブログを翻訳、要約して毎朝お届けします🎁`,
+  title: 'Techまとめ速報 | 5分でサクッと読めるテックメルマガ',
+  description: `最新情報を手軽に読みたい人、情報過多に疲れた人のための完全無料メルマガ📩 テクノロジー、プログラミング、AIに関する英語の最新記事やブログを「翻訳」「要約」して毎朝お届けします🎁 最新情報をもっとシンプルに`,
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
